c94525f8800171ae0d8987c3059ffecbfe3ab659
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-06-17 08:18:56 -0600

    Fixing location of workflow PGEs.

--- common/src/main/scripts/env.sh
+++ common/src/main/scripts/env.sh
@@ -5,7 +5,7 @@ export CATALINA_HOME=$LABCAS_HOME/apache-tomcat
 export SOLR_HOME=$LABCAS_HOME/solr-home
 export CATALINA_OPTS='-Dsolr.solr.home=$SOLR_HOME'
 # FIXME
-#export PGE_ROOT=$LABCAS_HOME/workflows/biomarker-discovery
+#export PGE_ROOT=$LABCAS_HOME/biomarker-discovery
 
 export FILEMGR_URL=http://localhost:9000
 export WORKFLOW_URL=http://localhost:9001
