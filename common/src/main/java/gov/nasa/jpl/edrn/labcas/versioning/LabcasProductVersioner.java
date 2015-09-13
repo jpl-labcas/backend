a6685e05dc690663801603f007c08f9bbdb98617
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-09-13 04:36:18 -0600

    Adding LabcasProductVersioner.

---
+++ common/src/main/java/gov/nasa/jpl/edrn/labcas/versioning/LabcasProductVersioner.java
@@ -0,0 +1,31 @@
+package gov.nasa.jpl.edrn.labcas.versioning;
+
+import org.apache.oodt.cas.filemgr.structs.Product;
+import org.apache.oodt.cas.filemgr.structs.exceptions.VersioningException;
+import org.apache.oodt.cas.filemgr.versioning.MetadataBasedFileVersioner;
+import org.apache.oodt.cas.metadata.Metadata;
+
+/**
+ * Class that defines the directory structure used to archive LabCAS products.
+ * Inspired by "gov.nasa.jpl.edrn.ecas.versioning.EDRNProductVersioner".
+ * 
+ * @author Luca Cinquini
+ */
+public class LabcasProductVersioner extends MetadataBasedFileVersioner {
+	
+	//private String filePathSpec = "/[SiteShortName]/[ProductReceivedDate]/[OrganName]/[InstrumentId]/[Filename]";
+	private String filePathSpec = "/[Dataset]/[Version]/[Filename]";
+	
+    public LabcasProductVersioner() {
+        setFlatProducts(true);
+        setFilePathSpec(filePathSpec);
+    }
+    
+    @Override
+    public void createDataStoreReferences(Product product, Metadata metadata) throws VersioningException {
+    	
+    	super.createDataStoreReferences(product, metadata);
+    	
+    }
+
+}
