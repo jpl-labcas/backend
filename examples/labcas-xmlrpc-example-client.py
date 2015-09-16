d164d16da182f403aaf2318199981814b9e72d51
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-09-16 09:01:24 -0600

    Adding example of XMLRPC python script.

---
+++ examples/labcas-xmlrpc-example-client.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# Example script for submitting and monitoring a 'labcas-upload' workflow
+
+import xmlrpclib
+import time
+
+# connect to Workflow Manager server
+# use verbose=True to print out every request/response 
+verbose=False
+server = xmlrpclib.ServerProxy('http://localhost:9001/', verbose=verbose)
+
+# list all supported events
+#server.workflowmgr.getRegisteredEvents()
+
+# list all supported workflows
+#server.workflowmgr.getWorkflows()
+
+# start 'labcas-upload' workflow
+#server.workflowmgr.handleEvent('labcas-upload', { 'Dataset':'mydata' } )
+
+#server.workflowmgr.getWorkflowInstances()
+
+# submit dynamic workflow, retrieve workflow instance id
+wInstId = server.workflowmgr.executeDynamicWorkflow( ['urn:edrn:LabcasUploadInitTask','urn:edrn:LabcasUploadExecuteTask'], { 'Dataset':'mydata' } )
+# wait for the server to instantiate this workflow before querying it
+time.sleep(1) 
+
+# use workflow instance id to check for status, wait until completed
+running_status  = ['CREATED', 'QUEUED', 'STARTED', 'PAUSED']
+pge_task_status = ['STAGING INPUT', 'BUILDING CONFIG FILE', 'PGE EXEC', 'CRAWLING']
+finished_status = ['FINISHED', 'ERROR']
+while (True):
+	response = server.workflowmgr.getWorkflowInstanceById(wInstId)
+        status = response['status']
+        if status in running_status or status in pge_task_status:
+        	print 'Workflow istance=%s running with status=%s' % (wInstId, status)
+        	time.sleep(1)
+	elif status in finished_status:
+		print 'Workflow istance=%s ended with status=%s' % (wInstId, status)
+		break
+	else:
+		print 'UNRECOGNIZED WORKFLOW STATUS: %s' % status
+		break
+print response
