79af885c846de34a9cd59351e71272ed64e05b2a
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-01-12 13:56:33 -0700

    Inserting example for labcas-test workflow.

---
+++ common/src/main/python/gov/nasa/jpl/edrn/labcas/test_workflow_client.py
@@ -0,0 +1,15 @@
+# Example script to execute the "labcas-test" workflow
+
+from gov.nasa.jpl.edrn.labcas.labcas_client import LabcasClient
+
+if __name__ == '__main__':
+    
+    # submit workflow
+    # ./wmgr-client --url http://localhost:9001 --operation --sendEvent --eventName labcas-test --metaData --key experiment 11 --key species snakes
+    labcasClient = LabcasClient()
+    wInstId = labcasClient.executeWorkflow(['urn:edrn:LabcasTestInit','urn:edrn:LabcasTestTask'], 
+                                           {'experiment':'11', 
+                                            'species':'snakes' } )
+
+    # monitor workflow instance
+    labcasClient.waitForCompletion(wInstId)
