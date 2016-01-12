63e1fb3a7e5b4e2536dac0297961efb659eab481
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-01-12 04:16:28 -0700

    Updating instructions and examples.

--- examples/labcas-upload-update-query-example.py
+++ examples/labcas-upload-update-query-example.py
@@ -66,10 +66,17 @@ def waitForCompletion(wInstId):
 			break
 	print response
 	
-def getProductType(dataset):
+def getProductTypeByName(datasetName):
 	
 	# retrieve a specific product type by name
-	productTypeDict =  fileManaferServerProxy.filemgr.getProductTypeByName(dataset)
+	productTypeDict =  fileManaferServerProxy.filemgr.getProductTypeByName(datasetName)
+	
+	printProductType(productTypeDict)
+	
+def getProductTypeById(datasetId):
+	
+	# retrieve a specific product type by name
+	productTypeDict =  fileManaferServerProxy.filemgr.getProductTypeById(datasetId)
 	
 	printProductType(productTypeDict)
 	
@@ -123,21 +130,23 @@ if __name__ == '__main__':
 	dataset = 'mydata'
 	
 	# upload dataset staged in directory 'mydata'
-	uploadDataset(dataset)
+	#uploadDataset(dataset)
 	
 	# upload the dataset again:
 	# o a new version will be generated
 	# o the product type metadata will be completey overridden
-	#uploadDataset(dataset)
+	uploadDataset(dataset)
 	
 	# update dataset metadata - no new version is generated
 	updateDataset(dataset)
 	
 	# list all product types in File manager
-	#listProductTypes()
+	listProductTypes()
 	
 	# query the product types from the XML/RPC File Manager interface
-	getProductType(dataset)
+	getProductTypeByName(dataset)
+	# or equivalently
+	getProductTypeById("urn:edrn:%s" % dataset)
 	
 	# list all products for given dataset == product type
 	listProducts(dataset)
