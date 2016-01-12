3230e581f40c08fa2c38a8cd48f5584dd0327752
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-01-12 08:19:17 -0700

    Improving NIST workflow.

--- common/src/main/java/gov/nasa/jpl/edrn/labcas/tasks/LabcasInitTaskInstance.java
+++ common/src/main/java/gov/nasa/jpl/edrn/labcas/tasks/LabcasInitTaskInstance.java
@@ -1,5 +1,6 @@
 package gov.nasa.jpl.edrn.labcas.tasks;
 
+import java.io.File;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -9,6 +10,8 @@ import org.apache.oodt.cas.metadata.Metadata;
 import org.apache.oodt.cas.workflow.structs.WorkflowTaskConfiguration;
 import org.apache.oodt.cas.workflow.structs.WorkflowTaskInstance;
 import org.apache.oodt.cas.workflow.structs.exceptions.WorkflowTaskInstanceException;
+
+import gov.nasa.jpl.edrn.labcas.Constants;
 
 /**
  * Task used to initialize a LabCAS workflow.
@@ -30,7 +33,26 @@ public class LabcasInitTaskInstance implements WorkflowTaskInstance {
 		Enumeration e = props.propertyNames();
 	    while (e.hasMoreElements()) {
 	      String key = (String) e.nextElement();
-	      LOG.log(Level.INFO, "\t==> config key=["+key+"] value=["+props.getProperty(key)+"]");
+	      String value = props.getProperty(key);
+	      LOG.log(Level.INFO, "\t==> config key=["+key+"] value=["+value+"]");
+	      
+	      // check existsence of files/directory inputs
+	      String stagingDir = System.getenv(Constants.ENV_LABCAS_STAGING);
+	      if (value.equalsIgnoreCase("directory") || value.equalsIgnoreCase("file"))  {
+	    	  String relPath = metadata.getMetadata(key);
+	    	  File file = new File(stagingDir, relPath);
+	    	  if (value.equalsIgnoreCase("file")) {
+	    		  if (!file.exists()) {
+	    			  throw new WorkflowTaskInstanceException("File "+relPath+" not found");
+	    		  }
+	    	  }
+	    	  if (value.equalsIgnoreCase("directory")) {
+	    		  if (!file.exists() || !file.isDirectory()) {
+	    			  throw new WorkflowTaskInstanceException("Directory "+relPath+" not found.");
+	    		  }
+	    	  }
+	      }
+	      
 	    }
 	    
 	    // print out metadata
@@ -39,7 +61,6 @@ public class LabcasInitTaskInstance implements WorkflowTaskInstance {
 			LOG.info("\t==> metadata key=["+key+"] value=["+metadata.getMetadata(key)+"]");
 		}
 
-		
 		
 	}
 
