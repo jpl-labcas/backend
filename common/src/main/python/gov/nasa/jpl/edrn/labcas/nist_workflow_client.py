6e80f6c498313d129b6f9c640dbef610d3db7a6b
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-02-29 11:50:25 -0700

    Updating the NIST Python example.

--- common/src/main/python/gov/nasa/jpl/edrn/labcas/nist_workflow_client.py
+++ common/src/main/python/gov/nasa/jpl/edrn/labcas/nist_workflow_client.py
@@ -5,13 +5,14 @@ from gov.nasa.jpl.edrn.labcas.labcas_client import LabcasClient
 if __name__ == '__main__':
     
     # submit workflow
-    # /wmgr-client --url http://localhost:9001 --operation --sendEvent --eventName nist --metaData --key pi Johns --key instrument trombone --key lab LAB01 --key date 20160101 --key mirnadatadir nist/LAB01/20160101
+    # ./wmgr-client --url http://localhost:9001 --operation --sendEvent --eventName nist 
+    #               --metaData --key DatasetId NIST_LAB01 --key LeadPI Johns --key Instrument trombone --key Lab LAB01 --key Date 20160101
     labcasClient = LabcasClient()
-    wInstId = labcasClient.executeWorkflow(['urn:edrn:NistInitTask','urn:edrn:NistConvertTask','urn:edrn:NistExecuteTask'], 
-                                           {'pi':'Johns', 
-                                            'instrument':'trumpet', 
-                                            'lab':'LAB01','date':'20160101', 
-                                            'mirnadatadir':'nist/LAB01/20160101' } )
-
+    wInstId = labcasClient.executeWorkflow(['urn:edrn:NistInitTask','urn:edrn:NistConvertTask','urn:edrn:NistExecTask','urn:edrn:NistCrawlTask'], 
+                                           {'DatasetId':'NIST_LAB01',
+                                            'LeadPI':'Johns', 
+                                            'Instrument':'trumpet', 
+                                            'Lab':'LAB01',
+                                            'Date':'20160101' } )
     # monitor workflow instance
     labcasClient.waitForCompletion(wInstId)
