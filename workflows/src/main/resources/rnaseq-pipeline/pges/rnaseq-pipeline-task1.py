dd5dfa441c07155fb36984e0cdff0812f8442cc6
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-06-07 04:40:11 -0600

    RNA sequence pipeline.

--- workflows/src/main/resources/rnaseq-pipeline/pges/rnaseq-pipeline-task1.py
+++ workflows/src/main/resources/rnaseq-pipeline/pges/rnaseq-pipeline-task1.py
@@ -5,13 +5,7 @@
 #                                          --data_dir /data/EDRN --output_dir thout --sample_id ERR164503
 
 import argparse
-import logging
 import os
-
-#from os import listdir, path, system
-#from os.path import isfile, join
-
-logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
     
@@ -21,21 +15,26 @@ if __name__ == '__main__':
     parser.add_argument('--gene_transcript_file', dest='gene_transcript_file', type=str, help="Gene Transcript File", default=None)
     parser.add_argument('--genome_index', dest='genome_index', type=str, help="Genome index", default=None)
     parser.add_argument('--data_dir', dest='data_dir', type=str, help="Data directory", default=None)
+    parser.add_argument('--exe_dir', dest='exe_dir', type=str, help="Executable directory", default=None)
     parser.add_argument('--output_dir', dest='output_dir', type=str, help="Output directory", default=None)
     parser.add_argument('--sample_id', dest='sample_id', type=str, help="Sample id", default=None)
     
     args_dict = vars( parser.parse_args() )
 
-    logging.debug('Number of threads=%s' % args_dict['num_threads'])
-    logging.debug('Gene transcripit file=%s' % args_dict['gene_transcript_file'])
-    logging.debug('Genome index=%s' % args_dict['genome_index'])
-    logging.debug('Data Directory=%s' % args_dict['data_dir'])
-    logging.debug('Output directory=%s' % args_dict['output_dir'])
-    logging.debug('Sample Id=%s' % args_dict['sample_id'])
+    print 'Number of threads=%s' % args_dict['num_threads']
+    print'Gene transcripit file=%s' % args_dict['gene_transcript_file']
+    print'Genome index=%s' % args_dict['genome_index']
+    print'Executable Directory=%s' % args_dict['exe_dir']
+    print'Data Directory=%s' % args_dict['data_dir']
+    print'Output directory=%s' % args_dict['output_dir']
+    print'Sample Id=%s' % args_dict['sample_id']
 
     # tophat -p 24 -G genes.gtf -o thout genome ERR164503_1.fastq ERR164503_2.fastq
-    command = "/Users/cinquini/bin/tophat -p %s -G %s -o %s %s" % (args_dict['num_threads'], args_dict['gene_transcript_file'], 
-                                               args_dict['output_dir'], args_dict['genome_index'])
+    command = "%s/tophat -p %s -G %s -o %s %s" % (args_dict['exe_dir'],
+                                                  args_dict['num_threads'], 
+                                                  args_dict['gene_transcript_file'], 
+                                                  args_dict['output_dir'], 
+                                                  args_dict['genome_index'])
 
     # loop for .fastq files in sample directory
     input_dir = os.path.join(args_dict['data_dir'], args_dict['sample_id'])
@@ -46,5 +45,5 @@ if __name__ == '__main__':
                 command += " %s" % f
 
     # execute command
-    logging.info("Executing command: %s" % command)
+    print "Executing command: %s" % command
     os.system( command )
