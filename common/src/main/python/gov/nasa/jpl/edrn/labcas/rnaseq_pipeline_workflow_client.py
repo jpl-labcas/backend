1e6f5dc586725e58b90a51d1fd6e77e9242abb7b
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-10-31 04:04:36 -0600

    Updating RnaSeq workflow.

--- common/src/main/python/gov/nasa/jpl/edrn/labcas/rnaseq_pipeline_workflow_client.py
+++ common/src/main/python/gov/nasa/jpl/edrn/labcas/rnaseq_pipeline_workflow_client.py
@@ -5,15 +5,24 @@ from gov.nasa.jpl.edrn.labcas.labcas_client import LabcasClient
 if __name__ == '__main__':
     
     # submit workflow
-    # ./wmgr-client --url http://localhost:9001 --operation --sendEvent --eventName rnaseq-pipeline 
-    #               --metaData --key genome_index genome --key sample_id ERR318895 --key OwnerPrincipal RnaResearchGroup
+    # ./wmgr-client --url http://localhost:9001 --operation --sendEvent --eventName rnaseq --metaData --key DatasetId ERR164503 --key Title LC_C1 --key AgeAtDiagnosis 45 --key Gender male --key SmokingStatus true --key Stage 1A --key ReferenceId geo:GSM993606 --key ReferenceUrl 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM993606' --key OrganSite Lung --key OwnerPrincipal RnaResearchGroup --key GenomeIndex genome
     labcasClient = LabcasClient()
-    wInstId = labcasClient.executeWorkflow(['urn:edrn:RnaSequencePipelineInit',
-                                            'urn:edrn:RnaSequencePipelineTask1',
-                                            'urn:edrn:RnaSequencePipelineTask2',
-                                            'urn:edrn:RnaSequencePipelineTask3'], 
-                                           {'genome_index':'genome',
-                                            'sample_id':'ERR164503', 
+    wInstId = labcasClient.executeWorkflow(['urn:edrn:RnaSeqInitTask',
+                                            'urn:edrn:RnaSequenceTask1',
+                                            'urn:edrn:RnaSequenceTask2',
+                                            'urn:edrn:RnaSequenceTask3',
+                                            'urn:edrn:RnaSeqCopyTask',
+                                            'urn:edrn:RnaSeqCrawlTask'], 
+                                           {'DatasetId':'ERR164503',
+                                            'Title':'LC_C1', 
+                                            'AgeAtDiagnosis':'45',
+                                            'Gender':'male',
+                                            'SmokingStatus':'true',
+                                            'Stage':'1A',
+                                            'ReferenceId':'geo:GSM993606',
+                                            'ReferenceUrl':'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM993606',
+                                            'OrganSite':'Lung',
+                                            'GenomeIndex':'genome',
                                             'OwnerPrincipal':'RnaResearchGroup' } )
     # monitor workflow instance
     labcasClient.waitForCompletion(wInstId)
