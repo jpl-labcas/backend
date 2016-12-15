2b61d3533d12e06c34010fbfa410c0ca78174d0a
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-12-15 14:46:23 -0700

    WAF workflow working.

--- common/src/main/python/gov/nasa/jpl/edrn/labcas/waf_workflow_client.py
+++ common/src/main/python/gov/nasa/jpl/edrn/labcas/waf_workflow_client.py
@@ -6,21 +6,21 @@ from gov.nasa.jpl.edrn.labcas.labcas_client import LabcasClient
 if __name__ == '__main__':
 
     # upload these datasets    
-    #datasetIds = ['UCHSC_1467', 'UCHSC_8798']
-    datasetIds = ['UCHSC_1467']
-    productType = 'University_of_Colorado_Lung_Image'
+    #dataset_names = ['UCHSC_1467', 'UCHSC_8798']
+    dataset_names = ['UCHSC_1467']
+    product_type = 'University_of_Colorado_Lung_Image'
     
-    for datasetId in datasetIds:
+    for dataset_name in dataset_names:
     
         # submit workflow
         # ./wmgr-client --url http://localhost:9001 --operation --sendEvent --eventName waf --metaData --key DatasetId UCHSC_1001
         labcasClient = LabcasClient()
         wInstId = labcasClient.executeWorkflow(['urn:edrn:WafInitTask',
                                                 'urn:edrn:WafCrawlTask'], 
-                                               { 'DatasetId':datasetId },
+                                               { 'DatasetName':dataset_name },
                                                )
         # monitor workflow instance
         labcasClient.waitForCompletion(wInstId)
         
         # list all products for given product type
-        labcasClient.listProducts(productType)
+        labcasClient.listProducts(product_type)
