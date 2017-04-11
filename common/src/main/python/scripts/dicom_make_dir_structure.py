d174ed31a62c09639833c453d666be808008b799
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2017-04-11 03:53:25 -0600

    Inserting script to reorganize dicom data.

---
+++ common/src/main/python/scripts/dicom_make_dir_structure.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from glob import glob
+import dicom
+
+# collection root directory:
+collection_dir = '/usr/local/labcas_staging/CBIS-DDSM/'
+
+# dataset directory
+dataset = sys.argv[1]
+dataset_dir = '/usr/local/labcas_staging/CBIS-DDSM/%s' % dataset
+
+# dataset version directory
+version_dir = '%s/1' % dataset_dir
+if not os.path.exists(version_dir):
+    os.makedirs(version_dir)
+
+# loop over DICOM files in dataset directory / DOI
+files = [y for x in os.walk(dataset_dir+"/DOI" ) for y in glob(os.path.join(x[0], '*.dcm'))]
+for f in files:
+
+   # extract file metadata
+   ds = dicom.read_file(f)
+   tag_names = ds.dir()
+   #for tag_name in tag_names:
+   #   data_element = ds.data_element(tag_name)
+      #print data_element
+   #   if tag_name != 'PixelData':
+   #       print 'key=%s --> value=%s' % (tag_name, data_element.value)
+   fid = ds.SOPInstanceUID
+
+   # move and rename DICOM file
+   src_path = os.path.abspath(f)
+   dst_path = '%s/%s.dcm' % (version_dir, fid)
+   print '\nMoving DICOM file=%s --> %s' % (src_path, dst_path)
+   if not os.path.exists(dst_path):
+      os.rename(src_path, dst_path)
