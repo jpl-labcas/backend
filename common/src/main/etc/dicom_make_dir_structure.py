8b83c5fb0d079dafd6900cf278ade35bcc092e1f
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2017-04-11 08:31:36 -0600

    Improving dicom script.

--- common/src/main/etc/dicom_make_dir_structure.py
+++ common/src/main/etc/dicom_make_dir_structure.py
@@ -8,7 +8,7 @@ collection_dir = '/usr/local/labcas/backend/staging/CBIS-DDSM/'
 
 # dataset directory
 dataset = sys.argv[1]
-dataset_dir = '/usr/local/labcas/backend/staging/CBIS-DDSM/%s' % dataset
+dataset_dir = '%s/%s' % (collection_dir, dataset)
 
 # dataset version directory
 version_dir = '%s/1' % dataset_dir
@@ -20,18 +20,23 @@ files = [y for x in os.walk(dataset_dir+"/DOI" ) for y in glob(os.path.join(x[0]
 for f in files:
 
    # extract file metadata
-   ds = dicom.read_file(f)
-   tag_names = ds.dir()
-   #for tag_name in tag_names:
-   #   data_element = ds.data_element(tag_name)
-      #print data_element
-   #   if tag_name != 'PixelData':
-   #       print 'key=%s --> value=%s' % (tag_name, data_element.value)
-   fid = ds.SOPInstanceUID
-
-   # move and rename DICOM file
    src_path = os.path.abspath(f)
-   dst_path = '%s/%s.dcm' % (version_dir, fid)
-   print '\nMoving DICOM file=%s --> %s' % (src_path, dst_path)
-   if not os.path.exists(dst_path):
-      os.rename(src_path, dst_path)
+   
+   try:
+       ds = dicom.read_file(f)
+       tag_names = ds.dir()
+       #for tag_name in tag_names:
+       #   data_element = ds.data_element(tag_name)
+          #print data_element
+       #   if tag_name != 'PixelData':
+       #       print 'key=%s --> value=%s' % (tag_name, data_element.value)
+       fid = ds.SOPInstanceUID
+    
+       # move and rename DICOM file
+       dst_path = '%s/%s.dcm' % (version_dir, fid)
+       print '\nMoving DICOM file=%s --> %s' % (src_path, dst_path)
+       if not os.path.exists(dst_path):
+          os.rename(src_path, dst_path)
+   except Exception as e:
+        print 'Error while processing file: %s' % src_path
+        print e
