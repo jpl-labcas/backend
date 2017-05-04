01fe63db420d591f5a973fafacaa9fc0b46efd58
EDRN <edrn@labcas-dev.jpl.nasa.gov>
2017-05-04 16:25:03 -0700

    Updating NLST script.

--- common/src/main/etc/dicom_nlst_make_dir_structure.py
+++ common/src/main/etc/dicom_nlst_make_dir_structure.py
@@ -7,13 +7,13 @@ from shutil import copyfile
 import dicom
 
 # data directories before and after re-organization
-source_dir = '/labcas-data/NLST-copy/'
-target_dir = '/labcas-data/NLST-copy-processed/'
-database_file = '%s/listing.txt' % target_dir
+source_dir = '/labcas-data/NLST/byID'
+target_dir = '/labcas-data/NLST-processed/'
+people_file = '%s/people.txt' % target_dir
 
-# open database file
-dbfile = open(database_file, 'w')
-dbfile.write("PatientID StudyInstanceUID SeriesInstanceUID SOPInstanceUID\n")
+# open people file
+peopledb = open(people_file, 'w')
+peopledb.write("PatientID\n")
 
 # loop over source sub-directories
 for subdir in os.listdir(source_dir):
@@ -24,6 +24,11 @@ for subdir in os.listdir(source_dir):
   target_subdir = '%s%s/1' % (target_dir, subdir)
   if not os.path.exists(target_subdir):
     os.makedirs(target_subdir)
+
+  # open person file
+  person_file = '%s%s/%s.txt' % (target_dir, subdir, subdir)
+  persondb = open(person_file, 'w')
+  persondb.write("PatientID StudyInstanceUID SeriesInstanceUID SOPInstanceUID\n")
 
   # loop recursively over all DICOM files in sub-directory
   src_files = [y for x in os.walk(source_subdir) for y in glob(os.path.join(x[0], '*.dcm'))]
@@ -36,7 +41,7 @@ for subdir in os.listdir(source_dir):
        tag_names = ds.dir()
        #for tag_name in tag_names:
        #   data_element = ds.data_element(tag_name)
-          #print data_element
+       #   print data_element
        #   if tag_name != 'PixelData':
        #       print 'key=%s --> value=%s' % (tag_name, data_element.value)
        series_id = ds.SeriesInstanceUID
@@ -47,16 +52,19 @@ for subdir in os.listdir(source_dir):
        dst_file = '%s/%s.dcm' % (target_subdir, file_id)
        if not os.path.exists(dst_file):
          print '\t --> %s' % (dst_file)
-       #   os.rename(src_file, dst_file)
-       # copyfile(src_file, dst_file)
+         # os.rename(src_file, dst_file)
+         copyfile(src_file, dst_file)
 
        # write file hierarchy into database file
-       dbfile.write("%s %s %s %s\n" % (subdir, study_id, series_id, dst_file) )
+       persondb.write("%s %s %s %s\n" % (subdir, study_id, series_id, dst_file) )
+       peopledb.write("%s\n" % subdir)
 
     except Exception as e:
         print 'Error while processing file: %s' % src_file
         print e
 
-    
-# clode database file
-dbfile.close()
+  # close person datanase file
+  persondb.close()
+
+# close people database file
+peopledb.close()
