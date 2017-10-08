c457a26d8a7f85c60b86217ea9fc204c82bc1fb2
Luca Cinquini <cinquini@LMC-038261.local>
2017-10-08 04:25:13 -0600

    Refactoring dicom_met_extractor.py

--- common/src/main/python/gov/nasa/jpl/edrn/labcas/server/dicom_met_extractor.py
+++ common/src/main/python/gov/nasa/jpl/edrn/labcas/server/dicom_met_extractor.py
@@ -1,32 +1,44 @@
-# Metadata extractor for DICOM files
-# Usage: python dicom_met_extractor.py <filepath.dcm>
-# Will extract metadata into the file: <filemath.dcm>.xmlmet
-# All metadata keys are prepended with "_File_" 
-# which will be removed before ingestion into the Solr index
-
 import sys
 import dicom
 
-dicom_filepath = sys.argv[1]
-met_filepath = dicom_filepath + ".xmlmet"
+def extract_metadata( dicom_filepath ):
+    ''' 
+    Metadata extractor for DICOM files.
+    Usage: python dicom_met_extractor.py <filepath.dcm>
+    Will extract metadata into the file: <filemath.dcm>.xmlmet.
+    All metadata keys are prepended with "_File_" 
+    which will be removed before ingestion into the Solr index
+    '''
+    
+    met_filepath = dicom_filepath + ".xmlmet"
 
-# read input file
-ds = dicom.read_file(dicom_filepath)
-tag_names = ds.dir()
+    # read input file
+    ds = dicom.read_file(dicom_filepath)
+    tag_names = ds.dir()
+    
+    # extract metadata, write output file
+    with open(met_filepath,'w') as file: 
+       file.write('<cas:metadata xmlns:cas="http://oodt.jpl.nasa.gov/1.0/cas">\n')
+    
+       # loop over input metadata fields
+       for tag_name in tag_names:
+          data_element = ds.data_element(tag_name)
+          if data_element:
+              if tag_name != 'PixelData' and tag_name!= 'LargestImagePixelValue' and tag_name != 'SmallestImagePixelValue': # skip binary data
+                 #print 'key=%s --> value=%s' % (tag_name, data_element.value)
+                 file.write('\t<keyval type="vector">\n')
+                 file.write('\t\t<key>_File_%s</key>\n' % str(tag_name))
+                 file.write('\t\t<val>%s</val>\n' % str(data_element.value))
+                 file.write('\t</keyval>\n')
+    
+       file.write('</cas:metadata>\n')
 
-# extract metadata, write output file
-with open(met_filepath,'w') as file: 
-   file.write('<cas:metadata xmlns:cas="http://oodt.jpl.nasa.gov/1.0/cas">\n')
-
-   # loop over input metadata fields
-   for tag_name in tag_names:
-      data_element = ds.data_element(tag_name)
-      if data_element:
-          if tag_name != 'PixelData' and tag_name!= 'LargestImagePixelValue' and tag_name != 'SmallestImagePixelValue': # skip binary data
-             #print 'key=%s --> value=%s' % (tag_name, data_element.value)
-             file.write('\t<keyval type="vector">\n')
-             file.write('\t\t<key>_File_%s</key>\n' % str(tag_name))
-             file.write('\t\t<val>%s</val>\n' % str(data_element.value))
-             file.write('\t</keyval>\n')
-
-   file.write('</cas:metadata>\n')
+if __name__ == "__main__":
+    '''
+    Example invocation: 
+    python gov/nasa/jpl/edrn/labcas/server/dicom_met_extractor.py /usr/local/labcas_archive/Sample_Mammography_Reference_Set/D0001/1/D0001_VOL_RMLO.dcm
+    '''
+    
+    dicom_filepath = sys.argv[1]
+    extract_metadata( dicom_filepath )
+    
