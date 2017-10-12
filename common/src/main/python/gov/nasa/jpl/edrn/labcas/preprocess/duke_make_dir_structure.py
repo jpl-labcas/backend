7e1f5f2af0d22882c2b1f441b4e93a0619cb316d
cinquini <luca.cinquini@jpl.nasa.gov>
2017-10-12 07:05:43 -0700

    Adding pre-processing script for Duke's data

--- common/src/main/python/gov/nasa/jpl/edrn/labcas/preprocess/duke_make_dir_structure.py
+++ common/src/main/python/gov/nasa/jpl/edrn/labcas/preprocess/duke_make_dir_structure.py
@@ -1,6 +1,5 @@
-# Python script to pre-process MOFFITT data:
+# Python script to pre-process Duke data:
 # o reorganizes the directory structire into LabCAS archive format
-# o builds the file description by using the MOFIITT filename conventions
 # o creates the dataset metadata file
 
 import os
@@ -9,18 +8,19 @@ from glob import glob
 from shutil import copyfile
 import dicom
 
-# process data from $LABCAS_ARCHIVE/Moffitt_BI --> $LABCAS_ARCHIVE/Sample_Mammography_Reference_Set
+# process data from $LABCAS_ARCHIVE/Duke --> $LABCAS_ARCHIVE/Sample_Mammography_Reference_Set
 COLLECTION_NAME = "Sample_Mammography_Reference_Set"
 TARGET_DATA_DIR=os.environ['LABCAS_ARCHIVE'] + "/" + COLLECTION_NAME
-SRC_DATA_DIR=os.environ['LABCAS_ARCHIVE'] + "/Moffitt_BI"
+SRC_DATA_DIR=os.environ['LABCAS_ARCHIVE'] + "/Duke"
 METADATA_DIR=os.environ['LABCAS_METADATA'] + "/" + COLLECTION_NAME
-INSTITUTION = "Moffitt"
+INSTITUTION = "Duke"
 
-def main():
+
+def main( dataset_id ):
         
     # dataset directory
-    #dataset = sys.argv[1]
-    dataset_id = 'D0001'
+    #dataset_id = sys.argv[1]
+    #dataset_id = 'D0001'
     src_dataset_dir = '%s/%s' % (SRC_DATA_DIR, dataset_id)
     target_dataset_dir = '%s/%s' % (TARGET_DATA_DIR, dataset_id)
     
@@ -30,7 +30,7 @@ def main():
         os.makedirs(target_version_dir)
             
     # create dataset metadata file
-    template_file = METADATA_DIR + "/TEMPLATE_Moffitt.cfg"
+    template_file = METADATA_DIR + "/TEMPLATE_Duke.cfg"
     dataset_archive_dir = TARGET_DATA_DIR + "/" + dataset_id
     if not os.path.exists(dataset_archive_dir):
         os.makedirs(dataset_archive_dir)
@@ -58,7 +58,10 @@ def main():
     
     
     # loop over DICOM files in dataset directory tree
+    print src_dataset_dir
     for root, dirs, files in os.walk(src_dataset_dir):
+
+      print dirs
     
       for filename in files:
         f = "%s/%s" % (root, filename)
@@ -89,4 +92,6 @@ def main():
             print e
 
 if __name__ == "__main__":
-    main()
+
+    dataset_id = "D0100"
+    main( dataset_id )
