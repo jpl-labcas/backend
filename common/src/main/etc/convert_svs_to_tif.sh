f7faf3aafd9b80f2a9af79ab2870b936ee78d689
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-07-23 10:12:27 -0600

    Updating svs to tif conversion script

--- common/src/main/etc/convert_svs_to_tif.sh
+++ common/src/main/etc/convert_svs_to_tif.sh
@@ -2,22 +2,36 @@
 # script to convert all .svs images in a directory tree to .tif
 
 #directory="/labcas-data/NLST-DATA-FROM-DARTMOUTH/nlst-path/134491"
+#directory="/labcas-data/NLST-DATA-FROM-DARTMOUTH/nlst-path/100147"
 directory="/labcas-data/NLST-DATA-FROM-DARTMOUTH/nlst-path"
 echo $directory
+outputdir="/labcas-data/NLST-DATA-FROM-DARTMOUTH/TIFs/"
 
 for f in $(find $directory -name '*.svs'); do
    echo $f
-   height="`openslide-show-properties $f | grep "openslide.level\[3\].height" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
-   width="`openslide-show-properties $f | grep "openslide.level\[3\].width" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
-   echo 'width: '$width' height: '$height
-   if [[ -n "$width" && -n "$height" ]]; then
-      filepath=`dirname "$f"`
-      filename=`basename "$f"`
-      pngfilename=${filename//svs/png}
-      tiffilename=${filename//svs/tif}
-      echo "Converting $filename --> $pngfilename --> $tiffilename"
-      openslide-write-png $f 0 0 3 $width $height $pngfilename
-      convert $pngfilename $tiffilename
-      /bin/rm $pngfilename
+   filepath=`dirname "$f"`
+   filename=`basename "$f"`
+   pngfilename=${filename//svs/png}
+   tiffilename=${filename//svs/tif}
+   echo "Converting $filename --> $pngfilename --> $tiffilename"
+
+   if [[ $filename =~ NLSI.* ]]; then
+     height="`openslide-show-properties $f | grep "openslide.level\[2\].height" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
+     width="`openslide-show-properties $f | grep "openslide.level\[2\].width" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
+     echo 'width: '$width' height: '$height
+     if [[ -n "$width" && -n "$height" ]]; then
+      openslide-write-png $f 0 0 2 $width $height $outputdir$pngfilename
+      convert $outputdir$pngfilename $outputdir$tiffilename
+      /bin/rm $outputdir$pngfilename
+     fi
+   else
+     height="`openslide-show-properties $f | grep "openslide.level\[3\].height" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
+     width="`openslide-show-properties $f | grep "openslide.level\[3\].width" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
+     echo 'width: '$width' height: '$height
+     if [[ -n "$width" && -n "$height" ]]; then
+      openslide-write-png $f 0 0 3 $width $height $outputdir$pngfilename
+      convert $outputdir$pngfilename $outputdir$tiffilename
+      /bin/rm $outputdir$pngfilename
+     fi
    fi
 done
