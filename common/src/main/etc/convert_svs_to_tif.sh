7f4bce68cc54c3ed8106cb7f6e8c7cc3f21078c6
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-07-24 03:59:19 -0600

    New version of svs conversion script.

--- common/src/main/etc/convert_svs_to_tif.sh
+++ common/src/main/etc/convert_svs_to_tif.sh
@@ -11,8 +11,12 @@ for f in $(find $directory -name '*.svs'); do
    echo $f
    filepath=`dirname "$f"`
    filename=`basename "$f"`
+   subdir=`basename "$filepath"`
+   mkdir -p "$outputdir$subdir"
    pngfilename=${filename//svs/png}
    tiffilename=${filename//svs/tif}
+   outputpng=${outputdir}${subdir}/${pngfilename}
+   outputtif=${outputdir}${subdir}/${tiffilename}
    echo "Converting $filename --> $pngfilename --> $tiffilename"
 
    if [[ $filename =~ NLSI.* ]]; then
@@ -20,18 +24,18 @@ for f in $(find $directory -name '*.svs'); do
      width="`openslide-show-properties $f | grep "openslide.level\[2\].width" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
      echo 'width: '$width' height: '$height
      if [[ -n "$width" && -n "$height" ]]; then
-      openslide-write-png $f 0 0 2 $width $height $outputdir$pngfilename
-      convert $outputdir$pngfilename $outputdir$tiffilename
-      /bin/rm $outputdir$pngfilename
+      openslide-write-png $f 0 0 2 $width $height $outputpng
+      convert $outputpng $outputtif
+      /bin/rm $outputpng
      fi
    else
      height="`openslide-show-properties $f | grep "openslide.level\[3\].height" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
      width="`openslide-show-properties $f | grep "openslide.level\[3\].width" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
      echo 'width: '$width' height: '$height
      if [[ -n "$width" && -n "$height" ]]; then
-      openslide-write-png $f 0 0 3 $width $height $outputdir$pngfilename
-      convert $outputdir$pngfilename $outputdir$tiffilename
-      /bin/rm $outputdir$pngfilename
+      openslide-write-png $f 0 0 3 $width $height $outputpng
+      convert $outputpng $outputtif
+      /bin/rm $outputpng
      fi
    fi
 done
