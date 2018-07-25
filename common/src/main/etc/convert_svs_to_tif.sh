32b40aadb95a85d1d23d45523ff5d5d186524824
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-07-25 14:20:34 -0600

    Latest copy of conversion script.

--- common/src/main/etc/convert_svs_to_tif.sh
+++ common/src/main/etc/convert_svs_to_tif.sh
@@ -8,10 +8,12 @@ echo $directory
 outputdir="/labcas-data/NLST-DATA-FROM-DARTMOUTH/TIFs/"
 
 for f in $(find $directory -name '*.svs'); do
+
    echo $f
    filepath=`dirname "$f"`
    filename=`basename "$f"`
    subdir=`basename "$filepath"`
+
    mkdir -p "$outputdir$subdir"
    pngfilename=${filename//svs/png}
    tiffilename=${filename//svs/tif}
@@ -19,23 +21,15 @@ for f in $(find $directory -name '*.svs'); do
    outputtif=${outputdir}${subdir}/${tiffilename}
    echo "Converting $filename --> $pngfilename --> $tiffilename"
 
-   if [[ $filename =~ NLSI.* ]]; then
-     height="`openslide-show-properties $f | grep "openslide.level\[2\].height" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
-     width="`openslide-show-properties $f | grep "openslide.level\[2\].width" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
-     echo 'width: '$width' height: '$height
-     if [[ -n "$width" && -n "$height" ]]; then
-      openslide-write-png $f 0 0 2 $width $height $outputpng
-      convert $outputpng $outputtif
-      /bin/rm $outputpng
-     fi
+   height="`openslide-show-properties $f | grep "openslide.level\[2\].height" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
+   width="`openslide-show-properties $f | grep "openslide.level\[2\].width" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
+   echo 'width: '$width' height: '$height
+   if [[ -n "$width" && -n "$height" ]]; then
+    openslide-write-png $f 0 0 2 $width $height $outputpng
+    convert $outputpng $outputtif
+    /bin/rm $outputpng
    else
-     height="`openslide-show-properties $f | grep "openslide.level\[2\].height" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
-     width="`openslide-show-properties $f | grep "openslide.level\[2\].width" | cut -f 2 -d ':' | cut -f 2 -d "'"`"
-     echo 'width: '$width' height: '$height
-     if [[ -n "$width" && -n "$height" ]]; then
-      openslide-write-png $f 0 0 2 $width $height $outputpng
-      convert $outputpng $outputtif
-      /bin/rm $outputpng
-     fi
+     echo "Error converting file $filename"
    fi
+
 done
