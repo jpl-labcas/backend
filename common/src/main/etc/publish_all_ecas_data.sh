92c50224c5f018da6823206df0c477b4008e8cde
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-08-29 04:50:00 -0600

    Working on ecas

--- common/src/main/etc/publish_all_ecas_data.sh
+++ common/src/main/etc/publish_all_ecas_data.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 # shell script to publish all ECAS data to LabCAS
+
 data_dir="/labcas-data/ecas"
 archive_dir="/usr/local/labcas/backend/archive"
+metadata_dir="/usr/local/labcas/src/ecas-metadata"
 
-# create symlinks from $LABCAS_ARCHIVE --> /labcas-data/ecas
+# activate Python labcas virtual environment
+source /data/local/labcas/labcas_venv/bin/activate
+
+# loop over all collections = datasets
 for source_dir in $data_dir/* ; do
 	
 	# extract dataset name = subdir name without the path
     dataset=$(basename "$source_dir")
     
-    # symlink target
+    # 1) create symlinks from $LABCAS_ARCHIVE --> /labcas-data/ecas
     target_dir="$archive_dir/$dataset"
-    
     if [ ! -d "$target_dir" ]; then
     	    echo "Symlinking $source_dir --> $target_dir"
  		ln -s $source_dir $target_dir
 	fi
+	
+	# 2) publish collection=dataset
+	
     
 done
+
 
 
 
