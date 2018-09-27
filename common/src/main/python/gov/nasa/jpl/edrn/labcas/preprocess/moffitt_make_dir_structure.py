69a8ca0102ac5e8752490b865b133f1aecf706a8
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-09-27 11:39:04 -0600

    Looping over sub-dirs.

--- common/src/main/python/gov/nasa/jpl/edrn/labcas/preprocess/moffitt_make_dir_structure.py
+++ common/src/main/python/gov/nasa/jpl/edrn/labcas/preprocess/moffitt_make_dir_structure.py
@@ -17,77 +17,83 @@ METADATA_DIR=os.environ['LABCAS_METADATA'] + "/" + COLLECTION_NAME
 INSTITUTION = "Moffitt"
 
 def main():
-        
-    # dataset directory
-    #dataset = sys.argv[1]
-    dataset_id = 'C0001'
-    src_dataset_dir = '%s/%s' % (SRC_DATA_DIR, dataset_id)
-    target_dataset_dir = '%s/%s' % (TARGET_DATA_DIR, dataset_id)
     
-    # dataset version directory
-    target_version_dir = '%s/1' % target_dataset_dir
-    if not os.path.exists(target_version_dir):
-        os.makedirs(target_version_dir)
+    # loop over sub-directories == ddatasets
+    subdirs = os.listdir(SRC_DATA_DIR)
+    for dataset_id in subdirs:
+        
+        print("Processing sub-directory: %s" % dataset_id)
+        
+        # dataset directory
+        #dataset = sys.argv[1]
+        #dataset_id = 'C0001'
+        src_dataset_dir = '%s/%s' % (SRC_DATA_DIR, dataset_id)
+        target_dataset_dir = '%s/%s' % (TARGET_DATA_DIR, dataset_id)
+        
+        # dataset version directory
+        target_version_dir = '%s/1' % target_dataset_dir
+        if not os.path.exists(target_version_dir):
+            os.makedirs(target_version_dir)
+                
+        # create dataset metadata file
+        template_file = METADATA_DIR + "/TEMPLATE_Moffitt.cfg"
+        dataset_archive_dir = TARGET_DATA_DIR + "/" + dataset_id
+        if not os.path.exists(dataset_archive_dir):
+            os.makedirs(dataset_archive_dir)
+        dataset_metadata_file =  dataset_archive_dir + "/" + dataset_id + ".cfg"
+    
+        if not os.path.exists(dataset_metadata_file):
             
-    # create dataset metadata file
-    template_file = METADATA_DIR + "/TEMPLATE_Moffitt.cfg"
-    dataset_archive_dir = TARGET_DATA_DIR + "/" + dataset_id
-    if not os.path.exists(dataset_archive_dir):
-        os.makedirs(dataset_archive_dir)
-    dataset_metadata_file =  dataset_archive_dir + "/" + dataset_id + ".cfg"
-
-    if not os.path.exists(dataset_metadata_file):
+           print('Creating dataset metadata file: %s' % dataset_metadata_file)
+    
+           # read in template metadata file
+           with open(template_file) as f:
+              metadata = f.read()
+    
+           # replace metadata
+           metadata = metadata.replace("DATASET_ID", dataset_id)
+           if dataset_id[0]=='D':
+               dataset_name = 'Dummy patient #%s (%s)' % (dataset_id[1:], INSTITUTION)
+           else:
+               dataset_name = 'Patient #%s (%s)' % (dataset_id[1:], INSTITUTION)
+           dataset_description = dataset_name + " mammography images"
+           metadata = metadata.replace("DATASET_NAME", dataset_name)
+           metadata = metadata.replace("DATASET_DESCRIPTION", dataset_description)
+           # write out metadata
+           with open(dataset_metadata_file, 'w') as f:
+              f.write(metadata)
         
-       print('Creating dataset metadata file: %s' % dataset_metadata_file)
-
-       # read in template metadata file
-       with open(template_file) as f:
-          metadata = f.read()
-
-       # replace metadata
-       metadata = metadata.replace("DATASET_ID", dataset_id)
-       if dataset_id[0]=='D':
-           dataset_name = 'Dummy patient #%s (%s)' % (dataset_id[1:], INSTITUTION)
-       else:
-           dataset_name = 'Patient #%s (%s)' % (dataset_id[1:], INSTITUTION)
-       dataset_description = dataset_name + " mammography images"
-       metadata = metadata.replace("DATASET_NAME", dataset_name)
-       metadata = metadata.replace("DATASET_DESCRIPTION", dataset_description)
-       # write out metadata
-       with open(dataset_metadata_file, 'w') as f:
-          f.write(metadata)
-    
-    
-    # loop over DICOM files in dataset directory tree
-    for root, dirs, files in os.walk(src_dataset_dir):
-    
-      for filename in files:
-        f = "%s/%s" % (root, filename)
-    
-        # extract file metadata
-        src_path = os.path.abspath(f)
-       
-        try:
-           ds = pydicom.read_file(f)
-           tag_names = ds.dir()
-           for tag_name in tag_names:
-              data_element = ds.data_element(tag_name)
-              if tag_name != 'PixelData' and data_element and data_element.value:
-                  print('key=%s --> value=%s' % (tag_name, data_element.value))
-           fid = ds.SOPInstanceUID
         
-           # move and rename DICOM file
-           # use DICOM identifier
-           #dst_path = '%s/%s.dcm' % (target_version_dir, fid)
-           # use original filename
-           dst_path = '%s/%s' % (target_version_dir, filename)
-           if not os.path.exists(dst_path):
-              print('\nCopying DICOM file=%s --> %s' % (src_path, dst_path))
-              copyfile(src_path, dst_path)
-                                         
-        except Exception as e:
-            print('Error while processing file: %s' % src_path)
-            print(e)
+        # loop over DICOM files in dataset directory tree
+        for root, dirs, files in os.walk(src_dataset_dir):
+        
+          for filename in files:
+            f = "%s/%s" % (root, filename)
+        
+            # extract file metadata
+            src_path = os.path.abspath(f)
+           
+            try:
+               ds = pydicom.read_file(f)
+               tag_names = ds.dir()
+               for tag_name in tag_names:
+                  data_element = ds.data_element(tag_name)
+                  #if tag_name != 'PixelData' and data_element and data_element.value:
+                  #    print('key=%s --> value=%s' % (tag_name, data_element.value))
+               fid = ds.SOPInstanceUID
+            
+               # move and rename DICOM file
+               # use DICOM identifier
+               #dst_path = '%s/%s.dcm' % (target_version_dir, fid)
+               # use original filename
+               dst_path = '%s/%s' % (target_version_dir, filename)
+               if not os.path.exists(dst_path):
+                  print('\nCopying DICOM file=%s --> %s' % (src_path, dst_path))
+                  copyfile(src_path, dst_path)
+                                             
+            except Exception as e:
+                print('Error while processing file: %s' % src_path)
+                print(e)
 
 if __name__ == "__main__":
     main()
